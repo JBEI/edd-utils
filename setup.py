3341f33bda5f6039de447cf90f80f36181f47ed2
Tijana <tradivojevic@lbl.gov>
2019-12-17 16:38:07 -0800

    Adding all authors

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="edd-utils",
     version="0.0.4",
-    author="Zak Costello, William Morrell, Tijana Radivojevic",
+    author="Zak Costello, William Morrell, Mark Forrer, Tijana Radivojevic",
     author_email="tradivojevic@lbl.gov",
     description="Download Studies from an Experiment Data Depot Instance",
     long_description=long_description,

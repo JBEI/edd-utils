e504d4c8c8ab4771f3bd6019ea0b369bbdd29464
Tijana <tradivojevic@lbl.gov>
2019-12-17 16:41:51 -0800

    New version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="edd-utils",
-    version="0.0.4",
+    version="0.0.5",
     author="Zak Costello, William Morrell, Mark Forrer, Tijana Radivojevic",
     author_email="tradivojevic@lbl.gov",
     description="Download Studies from an Experiment Data Depot Instance",
